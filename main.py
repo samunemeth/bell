30e38d148b410e693368b7a5e4e1367fe3c34f5d
Samu Németh <nemeth.samu.0202@gmail.com>
2024-05-01 10:53:28 +0200

    Changed logging format

--- main.py
+++ main.py
@@ -15,17 +15,18 @@ logging.info("Konfigurációk betöltése...")
 paths: dict[str] = []
 with open("paths.yaml") as paths_file:
     paths = yaml.safe_load(paths_file)
+
 # Ellenőrzés hogy léteznek-e a megadott fileok illetve mappák.
 for path_name, path in paths.items():
     if not file_exists(path):
-        logging.error("A \"" + str(path_name) + "\" kulcshoz megadott \"" + str(path) + "\" file vagy mappa nem létezik!")
-logging.debug("File helyek: " + str(paths))
+        logging.error("A '%s' kulcshoz megadott '%s' file vagy mappa nem létezik!", path_name, path)
+logging.debug("File helyek: %s", paths)
 
 # Időpontok importálása egy könyvtárba.
 events: dict[str, str] = []
 with open(paths["events"]) as events_file:
     events = yaml.safe_load(events_file)
-logging.debug("Események: " + str(events))
+logging.debug("Események: %s", events)
 
 # Funkció a különböző típusú események felismerésére és a megfelelő feladat futtatására.
 def run_event(event_type: str) -> None:
@@ -36,7 +37,7 @@ def run_event(event_type: str) -> None:
                 # playsound.playsound(paths["sounds-in"])
                 pass
             except playsound.PlaysoundException:
-                logging.warning("Becsengő sikertelen! A \"" + str(paths["sounds-in"]) + "\" file nem található.")
+                logging.warning("Becsengő sikertelen! A '%s' file nem található.", paths["sounds-in"])
             else:
                 logging.debug("Becsengő lejátszva.")
 
@@ -46,7 +47,7 @@ def run_event(event_type: str) -> None:
                 # playsound.playsound(paths["sounds-out"])
                 pass
             except playsound.PlaysoundException:
-                logging.warning("Kicsengő sikertelen! A \"" + str(paths["sounds-out"]) + "\" file nem található.")
+                logging.warning("Kicsengő sikertelen! A '%s' file nem található.", paths["sounds-out"])
             else:
                 logging.debug("Kicsengő lejátszva.")
 
@@ -54,7 +55,7 @@ def run_event(event_type: str) -> None:
             logging.info("Hirdetés sikeresen futtatva.")
 
         case _:
-            logging.warning("A bemeneti file nem megfelelően van formázva!, \"" + str(event_type) + "\" esemény nem létezik.")
+            logging.warning("A bemeneti file nem megfelelően van formázva!, '%s' esemény nem létezik.", event_type)
 
 # A konfigurációs fileban megadott események beidőzítése hétköznapokra.
 logging.info("Események időzítése...")
@@ -69,7 +70,7 @@ for event in events:
 
 # Logging
 logging.info("Inicializáció sikeres!")
-        
+
 # A beidőzített események végrehajtása.
 logging.info("Várakozás az eseményekre...")
 while True:
